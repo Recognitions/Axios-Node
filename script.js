49b7c00fcd73f7725d1940e0c275cbcfeed26906
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-02 15:06:41 -0300

    a

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+async function renderProducts(){
+    try{
+        const res = await axios.get("http://localhost:3000/products")
+        const products = res.data
+        const tbody = document.querySelector("tbody")
+        tbody.innerHTML=""
+        products.forEach(product => {
+            const tr = document.createElement("tr")
+            tr.innerHTML=`
+                <td>${product.name}</td>
+                <td>${product.price}</td>
+                <td>${product.desc}</td>
+                <td>
+                    <button type="button" class="btn warning">E</button>
+                    <button type="button" class="btn danger">X</button>
+                </td>
+            `
+            tbody.appendChild(tr)
+        });
+    }catch(e){}
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    renderProducts()
+})
+
 document.getElementById("product-form").addEventListener("submit", async (event)=>{
     event.preventDefault()
     const name = document.querySelector("#name")
@@ -6,11 +32,15 @@ document.getElementById("product-form").addEventListener("submit", async (event)
     try{
         const product = {
             name:name.value,
-            price:price.value,
+            price:parseInt(price.value),
             desc:desc.value
         }
         const createdProduct = await axios.post("http://localhost:3000/products",product)
         console.log(createdProduct.data)
+        renderProducts()
+
+        //const del = await axios.delete("http://localhost:3000/products/1")
+        //console.log(del)
     }catch(er){
         alert("erro")
         console.log(er)
